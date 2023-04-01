866cbd2006c3bee020818e5940bdd5690f1e9929
Samantha Genevay <samanthagenevay@gmail.com>
2023-03-31 20:50:06 -0700

    Completed Lab

--- server/app.py
+++ server/app.py
@@ -6,3 +6,41 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+@app.route("/")
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+
+@app.route("/print/<string:parameter>")
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+
+@app.route("/count/<int:number_param>")
+def count(number_param):
+    count = f""
+    for number in range(1,(number_param + 1)):
+        count += f'{number}\n'
+    return count
+
+
+@app.route("/math/<int:num1><string:operation><int:num2>")
+def math(num1, operation, num2):
+    if operation == "+":
+        return str(num1 + num2)
+    elif operation == "-":
+        return str(num1 - num2)
+    elif operation == "*":
+        return str(num1 * num2)
+    elif operation == "div":
+        return str(num1 / num2)
+    elif operation == "%":
+        return str(num1 % num2)
+    else:
+        return "Operation not recognized."
+
+
+
